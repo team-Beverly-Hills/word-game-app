bfe653c70480ec0ef62ae14fd3aaa89ac563bbeb
James Brooks <brooksjamesk01@gmail.com>
2022-02-28 16:55:26 -0800

    added starter JS code

---
+++ js/scifiStory.js
@@ -0,0 +1,22 @@
+'use strict';
+
+// Dom Windows
+let userName = document.getElementById('userName');
+// let story = document.getElementById('MadLibs Story');
+
+// localStorage Retrieval
+let savedUserName = localStorage.getItem('userName');
+// let storyProperties = localStorage.getItem('#### PLACEHOLDER ####');
+let parsedName = JSON.parse(savedUserName);
+// let parsedStory = JSON.parse(storyProperties);
+
+// Functions;
+function renderUserName(){
+  userName.textContent = `Welcome to the Site, ${parsedName}`;
+}
+
+renderUserName();
+
+// function renderStory() {
+
+// }
