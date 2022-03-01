2b00c8820aa3ba13ef07650c6cd3b7ecba52ea31
Abdinasir Yussuf <Abdinasir.yussuf@outlook.com>
2022-02-28 23:50:03 -0800

    redo

---
+++ js/adventureStory.js
@@ -0,0 +1,24 @@
+`use strict`;
+
+// window 
+let userName = document.getElementById('username');
+
+
+// retrieving from local storage
+let savedUserName = localStorage.getItem('username');
+let parsedName = JSON.parse(savedUserName);
+
+
+
+// now start functions
+
+
+
+// render function for the user name near logo
+
+
+
+
+
+
+// render function for story
