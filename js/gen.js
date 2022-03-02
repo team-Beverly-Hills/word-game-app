61edef90c3c475348ff837750c5ff1356068dc97
James Brooks <brooksjamesk01@gmail.com>
2022-03-02 10:50:43 -0800

    reworded a local storage item

--- js/gen.js
+++ js/gen.js
@@ -76,7 +76,7 @@ function handleSubmit(event){
   new Story(word1, word2, word3, word4, word5, word6, word7, word8, word9, word10, word11, word12, word13, word14, word15, word16, word17, word18, word19, word20, word21, word22);
 
   let localStories = JSON.stringify(stories);
-  localStorage.setItem('sci-fiStory', localStories);
+  localStorage.setItem('storyWords', localStories);
   storyForm.reset();
 }
 renderUserName();
